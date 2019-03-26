82404f9b7ec93730c1cde45f9050250c5d24439e
Rodrigo Baptista <rodrigombaptista@gmail.com>
2019-03-26 13:55:49 -0300

    Adding 'clean' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp')
-  ,imagemin = require('gulp-imagemin');
+  ,imagemin = require('gulp-imagemin')
+  ,clean = require('gulp-clean');
 
 gulp.task('copy', function(){
   gulp.src('src/img/**/*')
       .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-img', function() {
+gulp.task('clean', function(){
+  gulp.src('dist')
+      .pipe(clean());
+});
 
+gulp.task('build-img', function() {
   gulp.src('dist/img/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
+      .pipe(imagemin())
+      .pipe(gulp.dest('dist/img'));
 });
 
 
